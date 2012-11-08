c5abbb5ac42a91941d7b04597bf7e6d14780a7a4
Bing Deng <bingx.deng@intel.com>
2012-11-08 13:35:15 +0800

    ProgressBar: Fix error of process bar cannot update on some condition.
    When onDetachedFromWindow in processbar, if flag mRefreshIsPosted is true,
    we should set it to false. Otherwise, this flag will stay in true status,
    cannot turn to false anymore. At that condition, processbar cannot get
    update.
    
    Change-Id: I14c4e976b165ad737aae0a403a44822a7b3b2422
    Author: Liang Wang <liangx.wang@intel.com>
    Signed-off-by: Liang Wang <liangx.wang@intel.com>
    Signed-off-by: Bing Deng <bingx.deng@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 61090

--- core/java/android/widget/ProgressBar.java
+++ core/java/android/widget/ProgressBar.java
@@ -1838,9 +1838,7 @@ public class ProgressBar extends View {
         }
         if (mRefreshProgressRunnable != null) {
             removeCallbacks(mRefreshProgressRunnable);
-        }
-        if (mRefreshProgressRunnable != null && mRefreshIsPosted) {
-            removeCallbacks(mRefreshProgressRunnable);
+            mRefreshIsPosted = false;
         }
         if (mAccessibilityEventSender != null) {
             removeCallbacks(mAccessibilityEventSender);
