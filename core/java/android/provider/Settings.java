93770ab422288cd60d15c7bc88df57d86ca2318d
0xD34D <clark@scheffsblend.com>
2013-08-17 16:13:21 -0700

    [1/2] Add network stats to status bar
    Also includes
    http://gerrit.aokp.co/#/c/13356/
    http://gerrit.aokp.co/#/c/13360/
    
    PS2:
    Get the time delta between updates and use that to show
    average bytes per second.
    
    PS3:
    Code cleanup, change default text color and size to fit
    AOKP and add ability to change text color.
    
    PS4:
    Fix up layout for cleaner spacing and add for tablets
    
    PS8: Moved indicator before status icons
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/slim_dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    
    Change-Id: I86f37241d0f6217e9076b7d81bb19336e60a5a77

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3274,6 +3274,25 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
         /**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+        /**
+         * statusbar network stats text color
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_TEXT_COLOR = "status_bar_network_stats_text_color";
+
+        /**
          * use Alt Activity Resolver Grid
          * boolean
          *
