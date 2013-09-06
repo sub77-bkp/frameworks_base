ebd27eab2a009cd3ee10c78587e0a8b1037b54ea
fusionjack <fusionjack@fusionjack.com>
2013-09-06 22:05:09 +0200

    Disabled scrolling cache
    Change-Id: Ica7353e2b491743000dd3c788e5925d85ca11a3b

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -799,8 +799,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         boolean stackFromBottom = a.getBoolean(R.styleable.AbsListView_stackFromBottom, false);
         setStackFromBottom(stackFromBottom);
 
-        boolean scrollingCacheEnabled = a.getBoolean(R.styleable.AbsListView_scrollingCache, true);
-        setScrollingCacheEnabled(scrollingCacheEnabled);
+        setScrollingCacheEnabled(false);
 
         boolean useTextFilter = a.getBoolean(R.styleable.AbsListView_textFilterEnabled, false);
         setTextFilterEnabled(useTextFilter);
@@ -831,7 +830,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         setFocusableInTouchMode(true);
         setWillNotDraw(false);
         setAlwaysDrawnWithCacheEnabled(false);
-        setScrollingCacheEnabled(true);
+        setScrollingCacheEnabled(false);
 
         final ViewConfiguration configuration = ViewConfiguration.get(mContext);
         mTouchSlop = configuration.getScaledTouchSlop();
