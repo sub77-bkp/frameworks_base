f930965e97899c081938b646dd40eaa6e3cac7dc
xplodwild <xplod@ouverta.fr>
2013-09-26 14:39:39 +0100

    [1/2] Add battery level around unlock ring
    PS2:
    added missing change in KeyguardSelectorView.java
    
    Change-Id: Ia4dbc2335657c096c34349ec67c4adfec0aeb91b
    
    Conflicts:
    	core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3809,6 +3809,12 @@ public final class Settings {
         public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
 
         /**
+         * Whether or not to show circle battery around the lockscreen ring
+         * @hide
+         */
+        public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
