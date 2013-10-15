4297ffe08956bc4e5f26a852e75fba145ad98f61
0xD34D <clark@scheffsblend.com>
2013-10-15 11:55:18 -0700

    Active display: Allow apps to be excluded from being displayed [1/2]
    This has been a highly requested feature so I guess it's about time
    we implement it :).
    
    Change-Id: I5c334833b78325733ae076c07afccebb02758ead
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2646,6 +2646,13 @@ public final class Settings {
          */
         public static final String ACTIVE_DISPLAY_SUNLIGHT_MODE = "active_display_sunlight_mode";
 
+         /**
+         * A list of packages to exclude from being displayed in active display.
+         * This should be a string of packages separated by |
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_EXCLUDED_APPS = "active_display_excluded_apps";
+        
         /**
          * Whether lid wakes the device
          * @hide
