c91a4ab3d0688e31467a3acc19e850e2444244dc
Nalla Kartheek <karthe@codeaurora.org>
2014-02-17 13:29:21 +0530

    P2P : Flush P2P scan results on group fomation failed event
    P2P group fomation failed event removes the target device entry
    from peers list in framework.If Group negotiation request comes
    from supplicant, framework will drop request due to entry removal
    in peers list.
    To handle this situation, framework has to send P2P flush command upon
    device entry removal, so that supplicant and framework will be in sync
    with device information.
    
    Change-Id: I738ea50706d337bcc7e33d07bac53aa26b18ca7a
    CRs-Fixed: 610632

--- wifi/java/android/net/wifi/p2p/WifiP2pService.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pService.java
@@ -2704,6 +2704,7 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
             boolean peersChanged = mPeers.remove(mPeersLostDuringConnection);
             if (mPeers.remove(mSavedPeerConfig.deviceAddress) != null) {
                 peersChanged = true;
+                mWifiNative.p2pFlush();
             }
             if (peersChanged) {
                 sendPeersChangedBroadcast();
