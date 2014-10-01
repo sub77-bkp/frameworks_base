6f71c496fb31e9d0ce38933733b464bf88e930c7
Neil Fuller <nfuller@google.com>
2014-10-01 11:55:10 +0100

    Switch from FloatMath -> Math and Math.hypot where possible
    The motivation is an API change: FloatMath is going to be
    deprecated and/or removed. Performance is not the goal of
    this change.
    
    That said...
    
    Math is faster than FloatMath with AOT compilation.
    
    While making the change, occurances of:
    
    {Float}Math.sqrt(x * x + y * y) and
    {Float}Math.sqrt({Float}Math.pow(x, 2) + {Float}Math.pow(y, 2))
    
    have been replaced with:
    
    {(float)} Math.hypot(x, y)
    
    Right now there is no runtime intrinsic for hypot so is not faster
    in all cases for AOT compilation:
    
    Math.sqrt(x * x + y * y) is faster than Math.hypot(x, y) with
    AOT, but all other combinations of FloatMath, use of pow() etc.
    are slower than hypot().
    
    hypot() has the advantage of being self documenting and
    could be optimized in future. None of the behavior differences
    around NaN and rounding appear to be important for the cases
    looked at: they all assume results and arguments are in range
    and usually the results are cast to float.
    
    Different implementations measured on hammerhead / L:
    
    AOT compiled:
    
    [FloatMath.hypot(x, y)]
    benchmark=Hypot_FloatMathHypot} 633.85 ns; σ=0.32 ns @ 3 trials
    
    [FloatMath.sqrt(x*x + y*y)]
    benchmark=Hypot_FloatMathSqrtMult} 684.17 ns; σ=4.83 ns @ 3 trials
    
    [FloatMath.sqrt(FloatMath.pow(x, 2) + FloatMath.pow(y, 2))]
    benchmark=Hypot_FloatMathSqrtPow} 1270.65 ns; σ=12.20 ns @ 6 trials
    
    [(float) Math.hypot(x, y)]
    benchmark=Hypot_MathHypot} 96.80 ns; σ=0.05 ns @ 3 trials
    
    [(float) Math.sqrt(x*x + y*y)]
    benchmark=Hypot_MathSqrtMult} 23.97 ns; σ=0.01 ns @ 3 trials
    
    [(float) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))]
    benchmark=Hypot_MathSqrtPow} 156.19 ns; σ=0.12 ns @ 3 trials
    
    Interpreter:
    
    benchmark=Hypot_FloatMathHypot} 1180.54 ns; σ=5.13 ns @ 3 trials
    benchmark=Hypot_FloatMathSqrtMult} 1121.05 ns; σ=3.80 ns @ 3 trials
    benchmark=Hypot_FloatMathSqrtPow} 3327.14 ns; σ=7.33 ns @ 3 trials
    benchmark=Hypot_MathHypot} 856.57 ns; σ=1.41 ns @ 3 trials
    benchmark=Hypot_MathSqrtMult} 1028.92 ns; σ=9.11 ns @ 3 trials
    benchmark=Hypot_MathSqrtPow} 2539.47 ns; σ=24.44 ns @ 3 trials
    
    Bug: https://code.google.com/p/android/issues/detail?id=36199
    Change-Id: I06c91f682095e627cb547d60d936ef87941be692
    
    Conflicts:
    	core/java/android/widget/OverScroller.java
    	core/java/android/widget/Scroller.java
    
    Conflicts:
    	core/java/android/util/Spline.java
    	core/java/com/android/internal/widget/multiwaveview/MultiWaveView.java
    	graphics/java/android/graphics/ColorMatrix.java
    	services/java/com/android/server/power/DisplayPowerController.java
    	services/java/com/android/server/power/ElectronBeam.java
    	services/java/com/android/server/wm/WindowManagerService.java
    
    Conflicts:
    	tools/layoutlib/create/src/com/android/tools/layoutlib/create/CreateInfo.java

--- tools/layoutlib/create/src/com/android/tools/layoutlib/create/CreateInfo.java
+++ tools/layoutlib/create/src/com/android/tools/layoutlib/create/CreateInfo.java
@@ -230,7 +230,6 @@ public final class CreateInfo implements ICreateInfo {
         "android.os.SystemProperties",
         "android.text.AndroidBidi",
         "android.text.StaticLayout",
-        "android.util.FloatMath",
         "android.view.Display",
         "libcore.icu.DateIntervalFormat",
         "libcore.icu.ICU",
