2b45c13d9b4dc78ae6a975cf577005b5bb80f0d9
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:56:05 +0200

    frameworks/base: Bringup expanded desktop and implement per-user configuration.
    Change-Id: I27b2a4174f544f3437a5c918581ae599e4c3369b
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5506,6 +5506,11 @@ public class WindowManagerService extends IWindowManager.Stub
         mPointerEventDispatcher.unregisterInputEventListener(listener);
     }
 
+    @Override
+    public void addSystemUIVisibilityFlag(int flags) {
+        mLastStatusBarVisibility |= flags;
+    }
+
     // Called by window manager policy. Not exposed externally.
     @Override
     public int getLidState() {
