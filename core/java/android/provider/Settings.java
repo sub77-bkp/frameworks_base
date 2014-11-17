403d944ac440e26681289f5bc95e32620d6b6339
BigBrother1984 <carlosavignano@aospa.co>
2014-11-17 13:22:42 +0100

    SystemUI: Allow disabling adjusting volume sounds [1/2]
    * Option stored in Settings->Sounds & notification->Other settings.
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I0fc85725529637d1e9c5673fd267b23ae795583c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3380,6 +3380,12 @@ public final class Settings {
         public static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
 
         /**
+         * Whether the volume slider adjust sound is enabled. The value is
+         * boolean (1 or 0).
+         */
+        public static final String VOLUME_ADJUST_SOUND_ENABLED = "volume_adjust_sound_enabled";
+
+        /**
          * Whether custom hardware key rebinding is enabled
          * @hide
          */
@@ -3677,6 +3683,7 @@ public final class Settings {
             TTY_MODE,
             SOUND_EFFECTS_ENABLED,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             POWER_SOUNDS_ENABLED,       // moved to global
             DOCK_SOUNDS_ENABLED,        // moved to global
             LOCKSCREEN_SOUNDS_ENABLED,
@@ -3713,6 +3720,7 @@ public final class Settings {
         public static final String[] CLONE_TO_MANAGED_PROFILE = {
             DATE_FORMAT,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             SOUND_EFFECTS_ENABLED,
             TEXT_SHOW_PASSWORD,
             TIME_12_24
