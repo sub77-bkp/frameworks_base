23d13614ca060c5ce34fcb65dd8e43aebc38fce8
kecinzer <kecinzer@gmail.com>
2014-11-27 14:17:19 +0100

    Battery text mode (1/2)
    PS2: Make battery level more fit current solution
    PS3: Fix displaing battery level inside the icon
    PS6: Show battery level in status bar header when charging and percentage
         mode set to inside icon (it's replaced by charging icon)
    
    Change-Id: I537c99827e78c796b4980ec7751e5f9b58a88251
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3136,6 +3136,9 @@ public final class Settings {
 
         /**
          * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
          * @hide
          */
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
@@ -3205,7 +3208,7 @@ public final class Settings {
             POINTER_SPEED,
             VIBRATE_WHEN_RINGING,
             RINGTONE,
-            NOTIFICATION_SOUND
+            NOTIFICATION_SOUND,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
         };
 
