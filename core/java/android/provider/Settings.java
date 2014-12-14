e5ec05e89c05c047fdc6cb5f2a72e7b1ea7d4f6a
Ian Roy <ian.t.roy@gmail.com>
2014-12-13 19:05:13 -0500

    Forward port battery icon options [1/2]
    Bring forward the various battery icon options.
    Portrait, Landscape, Circle, Text.
    
    Still retains new options for where to display battery percentage text.
    
    Change-Id: Icfc14d989eebde9014538291c24aa76d328df234
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3134,6 +3134,18 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
+        /*
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 2: Display the battery as a circle
+         * 4: Hide the battery status information
+         * 5: Display the battery an icon in landscape mode
+         * 6: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
         /**
          * Status bar battery %
          * 0: Hide the battery percentage
@@ -3209,7 +3221,8 @@ public final class Settings {
             VIBRATE_WHEN_RINGING,
             RINGTONE,
             NOTIFICATION_SOUND,
-            STATUS_BAR_SHOW_BATTERY_PERCENT,
+            STATUS_BAR_BATTERY_STYLE,
+            STATUS_BAR_SHOW_BATTERY_PERCENT
         };
 
         /**
