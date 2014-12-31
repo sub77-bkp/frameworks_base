0497236514ccc712642e0fbe5906ee2ef3110b9a
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-12-31 20:30:40 +0100

    [1/4] Screen Recorder by 0xD34D
    http://forum.xda-developers.com/showthread.php?t=2547196
    
    @fusionjack:
    * Port to Lollipop
    * Remove VolumeUp + Power
    * Update icons. Thanks to @kantjer
    
    Change-Id: I38f4d12ef8fa7a1fba9cb3ff65feeeb152e1bad6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3721,6 +3721,24 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDE_ARROW = "network_traffic_hide_arrow";
 
         /**
+         * Width and height of output vide expressed in WxH
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
+
+        /**
+         * Screen recorder framerate in bits per second
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+        /**
+         * Whether to include audio when recording a video
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+
+        /**
          * Whether to use slim recents
          * @hide
          */
