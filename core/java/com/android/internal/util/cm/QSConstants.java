d677ab8f97f419ff9d4750bb7d23813c4ca1424a
Roman Birg <roman@cyngn.com>
2015-01-21 14:13:00 -0800

    SystemUI: add AudioFX/visualizer tile
    Change-Id: I78c5b95008183e664453529e5b2d3ba601f0d266
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/Android.mk
    	packages/SystemUI/res/values/cm_dimens.xml
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -38,6 +38,7 @@ public class QSConstants {
     public static final String TILE_LOCKSCREEN = "lockscreen";
     public static final String TILE_LTE = "lte";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
+    public static final String TILE_VISUALIZER = "visualizer";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -66,5 +67,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LOCKSCREEN);
         TILES_AVAILABLE.add(TILE_LTE);
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
+        TILES_AVAILABLE.add(TILE_VISUALIZER);
     }
 }
