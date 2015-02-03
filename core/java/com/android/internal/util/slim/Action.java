f3faeaad445476c52e6764839503820da778db15
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-02-03 22:06:54 +0100

    Action: Add kill-app and last-app actions
    Forward port from KitKat
    
    Change-Id: If8326ffed21faa2452e22b2afb717f4c123bd2a0

--- core/java/com/android/internal/util/slim/Action.java
+++ core/java/com/android/internal/util/slim/Action.java
@@ -113,6 +113,24 @@ public class Action {
                         new Intent("android.settings.SHOW_INPUT_METHOD_PICKER"),
                         new UserHandle(UserHandle.USER_CURRENT));
                 return;
+            } else if (action.equals(ActionConstants.ACTION_KILL)) {
+                if (isKeyguardShowing) {
+                    return;
+                }
+                try {
+                    barService.toggleKillApp();
+                } catch (RemoteException e) {
+                }
+                return;
+            } else if (action.equals(ActionConstants.ACTION_LAST_APP)) {
+                if (isKeyguardShowing) {
+                    return;
+                }
+                try {
+                    barService.toggleLastApp();
+                } catch (RemoteException e) {
+                }
+                return;
             } else if (action.equals(ActionConstants.ACTION_RECENTS)) {
                 if (isKeyguardShowing) {
                     return;
