931f88ad5714b2ad4c70670c8df1b1bf90cde971
Roman Birg <roman@cyngn.com>
2015-03-13 15:30:19 -0700

    SystemUI: load current user's qs tiles
    Change-Id: I29cd37d0daba3f25e8b7d07eb62df0ad344c9a80
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -24,6 +24,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -309,8 +310,8 @@ public class QSTileHost implements QSTile.Host {
     private List<String> loadTileSpecs() {
         final Resources res = mContext.getResources();
         final String defaultTileList = res.getString(R.string.quick_settings_tiles_default);
-        String tileList = Settings.Secure.getString(mContext.getContentResolver(),
-                Settings.Secure.QS_TILES);
+        String tileList = Settings.Secure.getStringForUser(mContext.getContentResolver(),
+                Settings.Secure.QS_TILES, UserHandle.USER_CURRENT);
         if (DEBUG) Log.d(TAG, "Config string: "+tileList);
         if (tileList == null) {
             tileList = res.getString(R.string.quick_settings_tiles);
