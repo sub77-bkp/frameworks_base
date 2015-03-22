5ef7caeadedef85b584fb9132cf94ba53d674bd4
Steve Kondik <steve@cyngn.com>
2015-03-22 12:23:35 -0700

    packagemanager: Use ArrayMap/ArraySet as per AOSP
     * To reduce memory consumption
    
    Change-Id: I85bdf84d4a507532e41fe29668fef9b4eabfb04a
    
    Conflicts:
    	services/core/java/com/android/server/pm/PackageManagerService.java
    	services/core/java/com/android/server/pm/PackageSettingBase.java

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -209,7 +209,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -414,7 +413,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     final int[] mGlobalGids;
     final SparseArray<ArraySet<String>> mSystemPermissions;
     final ArrayMap<String, FeatureInfo> mAvailableFeatures;
-    final ArrayMap<Signature, HashSet<String>> mSignatureAllowances;
+    final ArrayMap<Signature, ArraySet<String>> mSignatureAllowances;
 
     // If mac_permissions.xml was found for seinfo labeling.
     boolean mFoundPolicyFile;
@@ -2719,7 +2718,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     private boolean isAllowedSignature(PackageParser.Package pkg, String permissionName) {
         for (Signature pkgSig : pkg.mSignatures) {
-            HashSet<String> perms = mSignatureAllowances.get(pkgSig);
+            ArraySet<String> perms = mSignatureAllowances.get(pkgSig);
             if (perms != null && perms.contains(permissionName)) {
                 return true;
             }
@@ -2901,7 +2900,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             return PackageManager.SIGNATURE_NO_MATCH;
         }
 
-        // Since both signature sets are of size 1, we can compare without HashSets.
+        // Since both signature sets are of size 1, we can compare without ArraySets.
         if (s1.length == 1) {
             return s1[0].equals(s2[0]) ?
                     PackageManager.SIGNATURE_MATCH :
