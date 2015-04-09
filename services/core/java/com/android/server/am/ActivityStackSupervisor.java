8bbf03adfecdb6a2bf600f53aea3be314fdc4b74
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-04-09 20:17:04 +0200

    Revert "Frameworks: Slim heads up customizations for LP (1/2)"
    This reverts commit 8c576290a858def9e107a8e5d93fbda681df7b7c.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/statusbar/IStatusBar.aidl
    	core/java/com/android/internal/statusbar/IStatusBarService.aidl
    	core/res/res/values/slim_strings.xml
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/slim_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I9e47ff715acaad761fc96ad08f911a8b11226e0c

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -44,8 +44,6 @@ import android.app.IActivityContainer;
 import android.app.IActivityContainerCallback;
 import android.app.IActivityManager;
 import android.app.IApplicationThread;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProfilerInfo;
 import android.app.ActivityManager.RunningTaskInfo;
@@ -173,8 +171,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
     WindowManagerService mWindowManager;
     DisplayManager mDisplayManager;
 
-    private NotificationManager mNoMan;
-
     /** Identifier counter for all ActivityStacks */
     private int mLastStackId = HOME_STACK_ID;
 
@@ -301,11 +297,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
     }
 
     /**
-     * Is heads up currently enabled? Shared between ActivityStacks
-    */
-    String mHeadsUpPackageName = null;
-
-    /**
      * Is the privacy guard currently enabled? Shared between ActivityStacks
      */
     String mPrivacyGuardPackageName = null;
@@ -339,13 +330,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
             }
             return mStatusBarService;
-        }
-    }
-
-    private void initNotificationManager() {
-        if (mNoMan == null) {
-            mNoMan = (NotificationManager) mService.mContext
-                    .getSystemService(Context.NOTIFICATION_SERVICE);
         }
     }
 
@@ -4070,23 +4054,5 @@ public final class ActivityStackSupervisor implements DisplayListener {
         }
 
         return onLeanbackOnly;
-    }
-
-    void hideHeadsUpCandidate(String packageName) {
-        try {
-            IStatusBarService statusbar = getStatusBarService();
-            if (statusbar != null) {
-                statusbar.hideHeadsUpCandidate(packageName);
-            }
-        } catch (RemoteException e) {
-            // re-acquire status bar service next time it is needed.
-            mStatusBarService = null;
-        }
-    }
-
-    boolean getHeadsUpNotificationsEnabledForPackage(String packageName, int uid) {
-        initNotificationManager();
-        return mNoMan.getHeadsUpNotificationsEnabledForPackage(
-                packageName, uid) != Notification.HEADS_UP_NEVER;
     }
 }
